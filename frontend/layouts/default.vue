2c66f16196f5239c79d3239ac597e868f389792b
Atyson Jaime <atysonjaime@gmail.com>
2022-12-08 11:27:54 -0300

    :art: Ajusta layout geral

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -6,36 +6,16 @@
 <script>
 export default {
   name: 'DefaultLayout',
-  data() {
-    return {
-      items: [
-        {
-          title: 'Home',
-          icon: 'home',
-          to: { name: 'index' },
-        },
-        {
-          title: 'Inspire',
-          icon: 'lightbulb',
-          to: { name: 'inspire' },
-        },
-      ],
-    }
-  },
 }
 </script>
 
-<style lang="scss" scoped>
-html,
-body {
-  overflow: hidden !important;
-}
+<style lang="scss">
 .main-content {
   display: flex;
   justify-content: center;
   align-items: center;
   margin: 0;
-  padding: 0;
+  padding: 1.875rem;
   width: 100%;
   height: 100vh;
   background: #2980b9
@@ -46,6 +26,42 @@ body {
   -ms-animation: 10s linear 0s normal none infinite animate;
   -o-animation: 10s linear 0s normal none infinite animate;
   animation: 10s linear 0s normal none infinite animate;
+
+  .inside-content {
+    width: 100%;
+  }
+  .title {
+    font-size: 2.1875rem;
+    font-weight: 500;
+    line-height: 1.125;
+    color: #363636;
+    margin: 0;
+  }
+  .subtitle {
+    color: #7a7a7a;
+    font-size: 1.55rem;
+    font-weight: 400;
+    line-height: 1.25;
+    text-align: center;
+  }
+  .button-content {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    align-items: center;
+    flex-wrap: wrap;
+    padding: 0.9375rem;
+
+    a {
+      padding: 0.625rem;
+      button {
+        margin: 0;
+      }
+    }
+    button {
+      margin: 0.625rem;
+    }
+  }
 }
 @-webkit-keyframes animate {
   from {
