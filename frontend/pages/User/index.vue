6f1970a797e68be4160e9e865af79b55de807f06
Atyson Jaime <atysonjaime@gmail.com>
2022-12-08 23:12:24 -0300

    :sparkles: Adiciona página de usuário logado

---
+++ frontend/pages/User/index.vue
@@ -0,0 +1,51 @@
+<template lang="pug">
+.inside-content.box
+  <HeaderPage />
+  .body-content
+    .content-index
+      .content
+        p.subtitle Ola <b>{{this.user.name}}</b>
+      .button-content
+        b-button(type="is-dark is-large" rounded @click='logout') Deslogar
+        nuxt-link(to="/user/edit")
+          b-button(type="is-info is-large" rounded) Editar cadastro
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import HeaderPage from '~/components/HeaderPage.vue'
+export default {
+  component: {
+    HeaderPage,
+  },
+  head: {
+    title: 'CRUD User - User',
+  },
+  computed: {
+    ...mapState('auth', ['loggedIn', 'user']),
+  },
+  created() {
+    if (!this.loggedIn) {
+      this.$router.push('/login')
+      this.$toasted.global.defaultSuccess({
+        msg: 'Usuário deslogado',
+      })
+    }
+  },
+  methods: {
+    async logout() {
+      await this.$auth.logout()
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.inside-content {
+  max-width: 43.75rem;
+}
+.content-index {
+  width: 100%;
+  padding: 1.875rem;
+}
+</style>
